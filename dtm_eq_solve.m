18ba6ebc856c369e378ce6e22f7cecbf376cdbdd
fazildgr8 <mohamedfazilsulaiman@gmail.com>
2021-07-25 19:23:43 -0400

    RDTM Code done

--- dtm_eq_solve.m
+++ dtm_eq_solve.m
@@ -1,14 +1,26 @@
 clc;
-
-syms x1 x2 x y1 y2 y z1 z2 z a b c d d1 d2 h
-
-plane_eq = a*x + b*y + c*z - d == 0;
-
-% deq1 = sqrt((x1 - x)^2 + (y1 - y)^2 + (z1 - z)^2) - d1 == 0;
+% syms l m n;
 % 
-% deq2 = sqrt((x2 - x)^2 + (y2 - y)^2 + (z2 - z)^2) - d1 == 0;
+% a = 0.15;
+% b = 0.2;
+% c = 0.96;
+% theta = deg2rad(30);
+% 
+% 
+% lv = -1.2;
+% mv = 34.2;
+% nv = -7.15;
+% 
+% 
+% eq1 = mv*n - nv*m == sin(theta)*a;
+% 
+% eq2 = nv*l - lv*n == sin(theta)*b;
+% 
+% eq3 = lv*m - mv*l == sin(theta)*c;
+% 
+% eq4 = l*lv + m*mv + n*nv == cos(theta);
+% 
+% sol = solve([eq1, eq2, eq3, eq4],[l ,m n]);
 
-% deq3 = sqrt(x^2 + y^2 + z^2) - h == 0;
+syms x y z x1 y1 z1 x2 y2 z2 l1 m1 n1 l2 m2 n2;
 
-
-sol = solve([plane_eq,deq1,deq2,deq3],[x,y,z]);
